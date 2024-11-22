6177a3c63770f3c2447cd7089c029df1611a834c
LeosMacBookAir <xinletian2021@outlook.com>
2024-11-22 16:17:57 +0000

    Deployed extra file structure, including tabs, search and auth; Added presents:[require('nativewind/present')] as debug step; Implemented 2 pages of Aora

---
+++ metro.config.js
@@ -0,0 +1,6 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: "/Users/leo/Developer/React_Native/my-002-app/global.css" });
