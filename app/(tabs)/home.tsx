df14296eb19f5ef5f740fe6b812d1717b5c43dd2
LeosMacBookAir <xinletian2021@outlook.com>
2024-11-28 15:55:45 +0000

    Built layout and basic framwork for homepage, worked on displaying videos from appwrite on the app

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,33 +1,48 @@
-import { View, Text, FlatList, Image } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaFrameContext, SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants'
+import SearchInput from '@/components/SearchInput'
+import Trending from '@/components/Trending'
+import EmptyState from '@/components/EmptyState'
+import { getAllPosts } from '@/lib/appwrite'
+import useAppwrite from '@/lib/useAppwrite'
+import VideoCard from '@/components/VideoCard'
 
 const Home = () => {
-  return (
-    // Uses "FlatList" as framework for 
-    // containing the video contents 
 
-    <SafeAreaView className="bg-primary">
+  const { data : posts, refetch } = useAppwrite(getAllPosts)
+
+  const [refreshing, setRefreshing] = useState(false)
+  
+  const onRefresh = async() => {
+    setRefreshing(true)
+    await refetch()
+    setRefreshing(false)
+  }
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, ]}
+        data={posts}
         keyExtractor={(item) => item.$id}
         renderItem={({item}) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <VideoCard video={item} />
         )}
-
+        
         // Header of the list
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
-              <Text className="font-pmedium text-sm text-gray-100">
-                Welcome Back
-              </Text>
-              <Text className="text-2xl font-psemibold text-white">
-                UserName
-              </Text>
-            </View>
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  UserName
+                </Text>
+              </View>
 
             <View className='mt-1.5'>
               <Image 
@@ -38,11 +53,34 @@ const Home = () => {
 
               />
             </View>
+          </View>
+
+          <SearchInput />
+
+          <View className="w-full flex-1 pt-5 pb-8">
+            <Text className="text-gray-regular text-lg font-pregular mb-3">
+              Latest Videos
+            </Text>
+
+            <Trending posts={[{ id:1 }, { id:2 }, {id:3}] ?? []} />
+            
 
           </View>
+        </View>
         )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="Be the first one to upload a video!"
+          />
+        )}
+
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
+
       />
     </SafeAreaView>
+
+ 
   )
 }
 
